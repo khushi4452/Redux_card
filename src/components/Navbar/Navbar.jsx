7166ce3f3151ce9210d97a4f7b320ad33a950471
Musrat Khan <147485713+khushi4452@users.noreply.github.com>
2024-10-10 23:00:43 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,5 +2,5 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 function Navbar(){
-  const Item = use Selector((state) => 
+  const Item = use Selector((state) => state.cart);
 }
